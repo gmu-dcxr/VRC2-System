4ae2c347ea9d3cd4fcbd5366a8070588cea5af1f
Liuchuan Yu <lyu20@gmu.edu>
2023-06-09 11:14:31 -0400

    add RPC test

---
+++ Assets/Scripts/RPCTest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class RPCTest : NetworkBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Object.HasInputAuthority && Input.GetKeyDown(KeyCode.R))
+        {
+            RPC_SendMessage("Hey Mate!");
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    public void RPC_SendMessage(string message, RpcInfo info = default)
+    {
+        var _messages = "";
+
+        if (info.IsInvokeLocal)
+            message = $"You said: {message}\n";
+        else
+            message = $"Some other player said: {message}\n";
+        _messages = message;
+
+        Debug.LogWarning(_messages);
+    }
+}
