ab19e030677ce90d40725b55158d841a97b80d6b
Liuchuan Yu <lyu20@gmu.edu>
2023-06-12 15:38:08 -0400

    add menu and dialog

---
+++ Assets/Scripts/PipeMenuHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRC2
+{
+    public class PipeMenuHandler : MonoBehaviour
+    {
+        public void OnPickAPipe()
+        {
+            Debug.Log("You clicked Pick A pipe");
+        }
+    }
+}
