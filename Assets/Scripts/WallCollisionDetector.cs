54efdba537580aef296adae76e82b026651236c8
Liuchuan Yu <lyu20@gmu.edu>
2023-06-25 12:11:10 -0400

    fix pipe rotation when collision

---
+++ Assets/Scripts/WallCollisionDetector.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC2;
+
+public class WallCollisionDetector : MonoBehaviour
+{
+    // pipe's axes are different from wall's 
+    [Header("Pipe")] public float yRotationOffset = -90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        // get the game object
+        var go = other.gameObject;
+        if (go.CompareTag(GlobalConstants.pipeObjectTag))
+        {
+            HandlePipeCollision(go);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+        var go = other.gameObject;
+        if (go.CompareTag(GlobalConstants.pipeObjectTag))
+        {
+            HandlePipeCollision(go);
+        }
+    }
+
+    #region Handle Pipe's Collision with the Wall
+
+    void HandlePipeCollision(GameObject pipe)
+    {
+        // get the Interactable pipe
+        var ipipe = pipe.transform.parent.gameObject;
+        Debug.Log(ipipe.name);
+
+        var t = ipipe.transform;
+        var pos = t.position;
+        var rot = t.rotation.eulerAngles;
+        
+        // get the wall transform
+        var wt = gameObject.transform;
+        var wpos = wt.position;
+        var wrot = wt.rotation.eulerAngles;
+        
+        // set pipe's x rotation to the wall's x
+        rot.x = wrot.x;
+        // set pipe's y rotation to the wall's y rotation
+        rot.y = wrot.y + yRotationOffset;
+        // update
+        ipipe.transform.rotation = Quaternion.Euler(rot);
+        
+        // make 
+        
+    }
+
+
+
+
+    #endregion
+}
