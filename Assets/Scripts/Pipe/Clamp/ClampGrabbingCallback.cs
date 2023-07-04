f09d24eab583572ba45d9058c7f87270bcf8a891
Liuchuan Yu <lyu20@gmu.edu>
2023-07-04 16:37:07 -0400

    update clamp interactions

---
+++ Assets/Scripts/Pipe/Clamp/ClampGrabbingCallback.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace VRC2.Events
+{
+    [RequireComponent(typeof(GeneralGrabbable))]
+    public class ClampGrabbingCallback: MonoBehaviour
+    {
+        public GameObject clampGameObject;
+        private GeneralGrabbable _generalGrabbable;
+
+        private void Start()
+        {
+            _generalGrabbable = gameObject.GetComponent<GeneralGrabbable>();
+            
+            _generalGrabbable.OnSelect += OnSelect;
+            _generalGrabbable.OnUnselect += OnUnselect;
+        }
+
+        private void OnSelect()
+        {
+            
+        }
+        
+        private void OnUnselect()
+        {
+            
+        }
+    }
+}
