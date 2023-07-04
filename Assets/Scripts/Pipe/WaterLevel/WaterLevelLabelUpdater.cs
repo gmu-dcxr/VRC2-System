8a4c5af805d3a3bb755cf4a35fbf9a6d481fd27c
Liuchuan Yu <lyu20@gmu.edu>
2023-07-04 16:58:23 -0400

    add water level label

---
+++ Assets/Scripts/Pipe/WaterLevel/WaterLevelLabelUpdater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace VRC2.Events
+{
+    public class WaterLevelLabelUpdater : MonoBehaviour
+    {
+        [Header("Label")] public TextMeshPro textMeshPro;
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            var d = GetDegree();
+            textMeshPro.text = $"{d}";
+        }
+
+        int GetDegree()
+        {
+            var t = gameObject.transform.rotation.eulerAngles;
+            return (int)(Math.Abs(t.x % 90));
+        }
+    }
+}
