a6c446b87c21865f30039a4e06b84e98ef770d8f
Liuchuan Yu <lyu20@gmu.edu>
2023-07-05 12:20:10 -0400

    fix pipe connect pipe logic

--- Assets/Scripts/Pipe/PipeCollisionDetector.cs
+++ Assets/Scripts/Pipe/PipeCollisionDetector.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using NodeCanvas.Tasks.Actions;
+using Oculus.Interaction.DistanceReticles;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace VRC2.Events
 {
     public class PipeCollisionDetector : MonoBehaviour
     {
         [HideInInspector] public GameObject connecting;
+        private Object pipeParent;
+        
+        private bool connected = false;
 
         // to remove the seam
         private float eps = 1e-3f;
         private void Start()
         {
+            // pre-load object
+            pipeParent = AssetDatabase.LoadAssetAtPath(GlobalConstants.pipePipeConnectorPrefabPath, typeof(GameObject));
         }
 
         private void Update()
@@ -56,7 +64,7 @@ namespace VRC2.Events
                 // update rotation first, and update position in the next loop
                 connecting.transform.rotation = cip.rotation;
             }
-            else
+            else if (!connected)
             {
                 // update position
                 var cbounds = gameObject.GetComponent<Renderer>().bounds;
@@ -66,6 +74,31 @@ namespace VRC2.Events
                 var ed = cbounds.extents.x + obounds.extents.x - eps;
 
                 connecting.transform.position = cip.position + connecting.transform.right * ed;
+
+                var pos = cip.position + connecting.transform.right * ed / 2.0f;
+                var rot = cip.rotation;
+
+                var parentObject = Instantiate(pipeParent, pos, rot) as GameObject;
+
+                // update parent to make them move together
+                cip.transform.parent = parentObject.transform;
+                connecting.transform.parent = parentObject.transform;
+
+                // disable children's interactions
+                DisableInteraction(cip.gameObject);
+                DisableInteraction(connecting.gameObject);
+
+                connected = true;
+            }
+        }
+
+        void DisableInteraction(GameObject interactable)
+        {
+            // disable GrabInteractable who own ReticleDataIcon
+            var go = interactable.GetComponentInChildren<ReticleDataIcon>();
+            if (go != null)
+            {
+                go.gameObject.SetActive(false);
             }
         }
     }
--- Assets/Scripts/Static/GlobalConstants.cs
+++ Assets/Scripts/Static/GlobalConstants.cs
@@ -251,5 +251,13 @@ namespace VRC2
         #endregion
 
         #endregion
+
+        #region Pipe Connecting Pipe Parent Prefab
+        // connect two pipes into one pipe, this would be the parent object of two connected pipes
+        public static string pipePipeConnectorPrefabPath = "Assets/Prefabs/InteractablePipeContainer.prefab";
+
+
+
+        #endregion
     }
 }
