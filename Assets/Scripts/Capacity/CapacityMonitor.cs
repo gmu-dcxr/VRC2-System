e47954e19e97ad1a774742339903a6908bb38457
Liuchuan Yu <lyu20@gmu.edu>
2023-07-06 18:49:02 -0400

    update box clamp glue level grabbables

---
+++ Assets/Scripts/Capacity/CapacityMonitor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace VRC2
+{
+    public class CapacityMonitor: MonoBehaviour
+    {
+
+
+        public void OnUseGlue()
+        {
+            if (GlobalConstants.UseGlue())
+            {
+                // succeed
+            }
+            else
+            {
+                // fail because of used out
+            }
+        }
+
+        public void OnUseBox()
+        {
+            // box
+            if (GlobalConstants.UseBox())
+            {
+                // succeed
+            }
+            else
+            {
+                // fail because of used out
+            }
+        }
+
+        public void OnUsePipe()
+        {
+            
+        }
+
+        public void OnUseClamp()
+        {
+            // clamp
+            if (GlobalConstants.UseClamp())
+            {
+                // succeed
+            }
+            else
+            {
+                // fail because of used out
+            }
+        }
+    }
+}
--- Assets/Scripts/Pipe/Glue/GlueGrabbingCallback.cs
+++ Assets/Scripts/Pipe/Glue/GlueGrabbingCallback.cs
@@ -3,34 +3,19 @@ using UnityEngine;
 
 namespace VRC2.Events
 {
-    [RequireComponent(typeof(GeneralGrabbable))]
     public class GlueGrabbingCallback: MonoBehaviour
     {
         public GameObject horizontalGameObject;
         public GameObject verticalGameObject;
 
-        private GeneralGrabbable _generalGrabbable;
-
-        private void Start()
-        {
-            _generalGrabbable = gameObject.GetComponent<GeneralGrabbable>();
-            
-            _generalGrabbable.OnSelect += OnSelect;
-            _generalGrabbable.OnUnselect += OnUnselect;
-            
-            // default
-            horizontalGameObject.SetActive(false);
-            verticalGameObject.SetActive(true);
-        }
-
-        private void OnSelect()
+        public void OnSelect()
         {
             // enable horizontal object and disable vertical object to make it look real
             horizontalGameObject.SetActive(true);
             verticalGameObject.SetActive(false);
         }
 
-        private void OnUnselect()
+        public void OnUnselect()
         {
             // restore it
             horizontalGameObject.SetActive(false);
