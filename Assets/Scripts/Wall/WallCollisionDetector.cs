5c50dc36242243151489ada2b6c2aaf6ec497155
Liuchuan Yu <lyu20@gmu.edu>
2023-07-07 19:26:33 -0400

    fix clamp

--- Assets/Scripts/Wall/WallCollisionDetector.cs
+++ Assets/Scripts/Wall/WallCollisionDetector.cs
@@ -109,6 +109,13 @@ namespace VRC2
             // get the Interactable clamp
             var iclamp = clamp.transform.parent.gameObject;
             
+            // delete its rigid body
+            Rigidbody rb = null;
+            if (iclamp.TryGetComponent<Rigidbody>(out rb))
+            {
+                GameObject.Destroy(rb);
+            }
+
             // get offset
             var csi = iclamp.GetComponentInChildren<ClampScaleInitializer>();
             var offset = GlobalConstants.GetClampWallCollisionOffsetBySize(csi.clampSize);
