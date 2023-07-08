b3903ddf559d1078c71265bf811a341662f269a2
Liuchuan Yu <lyu20@gmu.edu>
2023-07-08 14:59:11 -0400

    update check clamp event

--- Assets/Scripts/Events/P1CheckClampEvent.cs
+++ Assets/Scripts/Events/P1CheckClampEvent.cs
@@ -2,11 +2,53 @@
 
 namespace VRC2.Events
 {
-    public class P1CheckClampEvent: BaseEvent
+    public class P1CheckClampEvent : BaseEvent
     {
+        public GameObject experimenter;
+        public GameObject clamp;
+        public GameObject destination;
+
+        private Vector3 startPosition;
+        private Vector3 clampPosition;
+        private Vector3 destPosition;
+
+        void Start()
+        {
+            startPosition = experimenter.transform.position;
+            clampPosition = clamp.transform.position;
+            destPosition = destination.transform.position;
+        }
+
+        public void MoveToDestination()
+        {
+            Debug.Log("MoveToDestination");
+            experimenter.transform.position = destPosition;
+        }
+
+        public void RefillClamp()
+        {
+            Debug.Log("RefillClamp");
+            GlobalConstants.currentClampCount = GlobalConstants.clampInitialCount;
+        }
+
+        public void BackToClampPosition()
+        {
+            Debug.Log("BackToClampPosition");
+            experimenter.transform.position = clampPosition;
+        }
+
+        public void MoveAway()
+        {
+            Debug.Log("MoveAway");
+            experimenter.transform.position = startPosition;
+        }
+
         public override void Execute()
         {
-            Debug.Log("Ask the experimenter to refill the clamp");
+            MoveToDestination();
+            BackToClampPosition();
+            RefillClamp();
+            MoveAway();
         }
     }
 }
