24b854c9952545577048b24922b6708dc5117a55
Liuchuan Yu <lyu20@gmu.edu>
2023-07-08 13:13:46 -0400

    add gravity for connecting pipe

---
+++ Assets/Scripts/Pipe/PipeContainerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Oculus.Interaction;
+using UnityEngine;
+namespace VRC2
+{
+    [RequireComponent(typeof(PointableUnityEventWrapper))]
+    public class PipeContainerManager: MonoBehaviour
+    {
+        private PointableUnityEventWrapper _wrapper;
+
+        private Rigidbody _rigidbody;
+        
+        private void Start()
+        {
+            _rigidbody = gameObject.GetComponent<Rigidbody>();
+            _wrapper = gameObject.GetComponent<PointableUnityEventWrapper>();
+            
+            _wrapper.WhenUnselect.AddListener(OnUnselect);
+            _wrapper.WhenSelect.AddListener(OnSelect);
+        }
+
+        void OnUnselect()
+        {
+            // enable kinematic
+            _rigidbody.isKinematic = false;
+        }
+
+        void OnSelect()
+        {
+            _rigidbody.isKinematic = true;
+        }
+    }
+}
