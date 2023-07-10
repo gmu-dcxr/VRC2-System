55d2385140fa5505f7fd2e2820b4c3500a198e0d
Liuchuan Yu <lyu20@gmu.edu>
2023-07-09 22:06:53 -0400

    Fix RobotBendCut menu

---
+++ Assets/Scripts/Pipe/PipeHelper.cs
@@ -0,0 +1,54 @@
+﻿using Oculus.Interaction;
+using Oculus.Interaction.DistanceReticles;
+using UnityEngine;
+using VRC2.Events;
+
+
+namespace VRC2.Pipe
+{
+    public static class PipeHelper
+    {
+        public static void BeforeMove(ref GameObject interactablePipe)
+        {
+            // remove rigid body, interactable, and collision detector
+
+            // remove its rigid body
+            var rb = interactablePipe.GetComponent<Rigidbody>();
+            GameObject.Destroy(rb);
+
+            // disable its interactable ability
+            var reticle = interactablePipe.GetComponentInChildren<ReticleDataIcon>();
+            reticle.gameObject.SetActive(false);
+
+            // disable pipe collision detector
+            var pcd = interactablePipe.GetComponentInChildren<PipeCollisionDetector>(false);
+            pcd.enabled = false;
+        }
+
+        public static void AfterMove(ref GameObject interactablePipe)
+        {
+            // restore rigid body if need
+            var rb = interactablePipe.GetComponent<Rigidbody>();
+            if (rb == null)
+            {
+                // add new one
+                rb = interactablePipe.AddComponent<Rigidbody>();
+            }
+
+            // enable interactable ability
+            var reticle = interactablePipe.GetComponentInChildren<ReticleDataIcon>(true);
+            reticle.gameObject.SetActive(true);
+
+            // enable collision detector
+            var pcd = interactablePipe.GetComponentInChildren<PipeCollisionDetector>(false);
+            pcd.enabled = true;
+
+            // update rigid body
+            var si = interactablePipe.GetComponent<SnapInteractor>();
+            si.InjectRigidbody(rb);
+
+            var dgi = interactablePipe.GetComponentInChildren<DistanceGrabInteractable>();
+            dgi.InjectRigidbody(rb);
+        }
+    }
+}
