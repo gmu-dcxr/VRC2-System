c63a0605132103a4a633cdbf93001f54517c9acb
Liuchuan Yu <lyu20@gmu.edu>
2023-07-10 15:12:58 -0400

    fix water level and distance label

---
+++ Assets/Scripts/Authority/AuthoritySettingHook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace VRC2.Authority
+{
+    public class AuthoritySettingHook : MonoBehaviour
+    {
+        [Header("Authority")] public bool P1Only;
+        public bool P2Only;
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            // return is game not started
+            if (!GlobalConstants.IsNetworkReady()) return;
+
+
+            // enable on both sides
+            if (!P1Only && !P2Only) return;
+
+            // disable P2 side
+            if (P1Only && GlobalConstants.Checker)
+            {
+                gameObject.SetActive(false);
+            }
+
+            // disable P1 side
+            if (P2Only && GlobalConstants.Checkee)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
