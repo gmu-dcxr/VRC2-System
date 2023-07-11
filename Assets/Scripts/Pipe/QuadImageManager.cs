997707409ac231e24d8db42d628c08cac5f17087
Liuchuan Yu <lyu20@gmu.edu>
2023-07-11 16:18:13 -0400

    add markers on the wall

---
+++ Assets/Scripts/Pipe/QuadImageManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRC2
+{
+    public class QuadImageManager : MonoBehaviour
+    {
+        [Header("Image")] [Tooltip("Folder name under the Assets/Resource folder")]
+        public string folder;
+
+        [Tooltip("No filetype extension, e.g., using `image` not `image.png`")]
+        public string filename;
+
+        private Texture2D _texture;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetImageAsTexture();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void SetImageAsTexture()
+        {
+            var name = $"{folder}/{filename}";
+            _texture = Resources.Load<Texture2D>(name);
+            _texture.alphaIsTransparency = true;
+            Debug.Log(_texture.dimension);
+            gameObject.GetComponent<MeshRenderer>().materials[0].mainTexture = _texture;
+        }
+    }
+}
