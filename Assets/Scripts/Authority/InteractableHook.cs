a63ce093f5e4f72d2bf835f2dd3394c565fb43c2
Liuchuan Yu <lyu20@gmu.edu>
2023-07-24 16:26:43 -0400

    update waterlevel

---
+++ Assets/Scripts/Authority/InteractableHook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Oculus.Interaction;
+using Oculus.Interaction.DistanceReticles;
+using UnityEngine;
+
+namespace VRC2.Authority
+{
+    public class InteractableHook: AuthorityHook
+    {
+        private GameObject interactable;
+
+        private void Start()
+        {
+            interactable = gameObject.GetComponentInChildren<ReticleDataIcon>().gameObject;
+        }
+
+        public override void DisableP1()
+        {
+            interactable.SetActive(false);
+        }
+
+        public override void DisableP2()
+        {
+            
+        }
+    }
+}
