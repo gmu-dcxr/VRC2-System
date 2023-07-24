303f1fa30a375aebf6e7e99cfcc139de3c1f3109
Liuchuan Yu <lyu20@gmu.edu>
2023-07-24 11:28:51 -0400

    update

--- Assets/Scripts/Authority/GameObjectHook.cs
+++ Assets/Scripts/Authority/GameObjectHook.cs
@@ -3,38 +3,17 @@ using UnityEngine;
 
 namespace VRC2.Authority
 {
-    public class AuthoritySettingHook : AuthorityHook
+    public class GameObjectHook : AuthorityHook
     {
-        [Header("Authority")] public bool P1Only;
-        public bool P2Only;
 
-        private void Start()
+        public override void DisableP1()
         {
-
+            gameObject.SetActive(false);
         }
-        
-        
 
-        private void Update()
+        public override void DisableP2()
         {
-            // return is game not started
-            if (!GlobalConstants.IsNetworkReady()) return;
-
-
-            // enable on both sides
-            if (!P1Only && !P2Only) return;
-
-            // disable P2 side
-            if (P1Only && GlobalConstants.Checker)
-            {
-                gameObject.SetActive(false);
-            }
-
-            // disable P1 side
-            if (P2Only && GlobalConstants.Checkee)
-            {
-                gameObject.SetActive(false);
-            }
+            gameObject.SetActive(false);
         }
     }
 }
