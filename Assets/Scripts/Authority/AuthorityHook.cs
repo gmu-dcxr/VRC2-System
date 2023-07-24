b6186f3cb9896d340f7e6de475d7041ca2748027
Liuchuan Yu <lyu20@gmu.edu>
2023-07-24 11:35:33 -0400

    update AuthorityHook

--- Assets/Scripts/Authority/AuthorityHook.cs
+++ Assets/Scripts/Authority/AuthorityHook.cs
@@ -9,6 +9,8 @@ namespace VRC2.Authority
         [Header("Authority")] public bool P1Only;
         public bool P2Only;
 
+        private bool _authorityUpdated = false;
+
         private NetworkRunner _runner = null;
         public virtual void DisableP2()
         {
@@ -23,6 +25,8 @@ namespace VRC2.Authority
 
         private void Update()
         {
+            if(_authorityUpdated) return;
+            
             if (_runner == null)
             {
                 _runner = FindObjectOfType<NetworkRunner>();
@@ -36,12 +40,14 @@ namespace VRC2.Authority
             // disable P2 side
             if (P1Only && _runner.IsClient)
             {
+                _authorityUpdated = true;
                 DisableP2();
             }
 
             // disable P1 side
             if (P2Only &&  _runner.IsServer)
             {
+                _authorityUpdated = true;
                 DisableP1();
             }
         }
