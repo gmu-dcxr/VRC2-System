1aeed2691947c0dfdaf917c7e2ef5f9b5641e2ec
Liuchuan Yu <lyu20@gmu.edu>
2023-07-24 18:02:20 -0400

    update WIP scene

---
+++ Assets/Scripts/Authority/InteractableLevelHook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Oculus.Interaction;
+using Oculus.Interaction.DistanceReticles;
+using UnityEngine;
+using VRC2.Pipe;
+using VRC2.Scenarios;
+
+namespace VRC2.Authority
+{
+    public class InteractableLevelHook : AuthorityHook
+    {
+        public override void DisableP1()
+        {
+            PipeHelper.DisableInteraction(gameObject);
+        }
+
+        public override void DisableP2()
+        {
+
+        }
+    }
+}
