4e54cb0976d0badca5e070d30f9231efbcf99be6
Liuchuan Yu <lyu20@gmu.edu>
2023-07-30 17:53:39 -0400

    update prefabs

--- Assets/Scripts/Pipe/PipeManipulation.cs
+++ Assets/Scripts/Pipe/PipeManipulation.cs
@@ -179,6 +179,32 @@ namespace VRC2
             sb.x = fb;
 
             segmentB.transform.localScale = sb;
+
+            // update texture
+            UpdateTexture(segmentA, fa);
+            UpdateTexture(segmentB, fb);
+        }
+
+        void UpdateTexture(GameObject pipe, float xValue, float yValue = 1.0f)
+        {
+            // By: Will @ willfredranc@gmail.com
+
+            //Pipe with texture
+            //Scale texture on given pipe based on x value of pipe scale.
+            //check pipe type (water has multiple textures)
+            // float xValue = pipe.transform.localScale.x;
+            // float yValue = pipe.transform.localScale.y;
+            Renderer r = pipe.GetComponent<Renderer>();
+            //material of pipe, changes to this only effect this object. ONLY AT RUNTIME
+            Material m = r.material;
+            m.SetTextureScale("_MainTex", new Vector2(xValue, yValue));
+            //Having offset set between values 0.1-0.4, inclusive, seemed to properly align texture. 
+            m.SetTextureOffset("_MainTex", new Vector2(0.1f, 0.0f));
+
+            m.SetTextureScale("_BumpMap", new Vector2(xValue, yValue));
+            m.SetTextureOffset("_BumpMap", new Vector2(0.1f, 0.0f));
+            m.SetTextureScale("_ParallaxMap", new Vector2(xValue, yValue));
+            m.SetTextureOffset("_ParallaxMap", new Vector2(0.1f, 0.0f));
         }
 
         #region Pointable Event
