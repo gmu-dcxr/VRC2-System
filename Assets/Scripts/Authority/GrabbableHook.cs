62bcf376a6d0355d2dbe009c814786bf38104352
Liuchuan Yu <lyu20@gmu.edu>
2023-08-02 15:20:38 -0400

    add GrabbableHook for glue lid

---
+++ Assets/Scripts/Authority/GrabbableHook.cs
@@ -0,0 +1,28 @@
+﻿using Oculus.Interaction.DistanceReticles;
+using UnityEngine;
+
+namespace VRC2.Authority
+{
+    public class GrabbableHook : AuthorityHook
+    {
+        private GameObject grabInteractable
+        {
+            get
+            {
+                var rdi = gameObject.GetComponentInChildren<ReticleDataIcon>();
+                if (rdi != null) return rdi.gameObject;
+                return null;
+            }
+        }
+
+        public override void DisableP1()
+        {
+            grabInteractable?.SetActive(false);
+        }
+
+        public override void DisableP2()
+        {
+            grabInteractable?.SetActive(false);
+        }
+    }
+}
