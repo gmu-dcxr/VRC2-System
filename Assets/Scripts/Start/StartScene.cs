a4f6c64dd686bdf57498336bc3b930d986e9eb05
Ian Lang <ilang@gmu.edu>
2023-08-02 21:46:46 -0400

    StartScene
    I left most assets in from the main menu to make it easier to replace them with the ones we would like to have.

---
+++ Assets/Scripts/Start/StartScene.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRC2;
+
+public class StartScene : MonoBehaviour
+{
+    public string sceneToGoTo = "WIP";
+    private NetworkRunner _runner;
+    private bool gameStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HostButton()
+    {
+        if (!gameStarted)
+        {
+            GlobalConstants.GameStarted = true;
+            GlobalConstants.Checker = false; // P1
+            StartGame(GameMode.Host);
+            SceneManager.LoadScene(sceneToGoTo);
+        }
+    }
+    public void JoinButton()
+    {
+        if (!gameStarted)
+        {
+            GlobalConstants.GameStarted = true;
+            GlobalConstants.Checker = true; // P2
+            StartGame(GameMode.Client);
+            SceneManager.LoadScene(sceneToGoTo);
+        }
+    }
+    public async void StartGame(GameMode mode)
+    {
+        var result = await _runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "VRC2",
+            CustomLobbyName = "VRC2",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>(),
+            // PlayerCount = 2,
+            DisableClientSessionCreation = true
+        });
+
+        if (result.Ok)
+        {
+            // all good
+            GlobalConstants.networkRunner = _runner;
+            gameStarted = true;
+        }
+        else
+        {
+            Debug.LogError($"Failed to Start: {result.ShutdownReason}");
+        }
+    }
+}
