577bc79f95d0f5e4689f59756d48360e05249488
Liuchuan Yu <lyu20@gmu.edu>
2023-08-03 17:15:53 -0400

    fix BaselineS2

--- Assets/Scripts/Scenarios/ScenarioFactory/BaselineS2.cs
+++ Assets/Scripts/Scenarios/ScenarioFactory/BaselineS2.cs
@@ -1,7 +1,7 @@
 ﻿using Assets.OVR.Scripts;
 using TMPro;
 using UnityEngine;
-
+using UnityTimer;
 
 namespace VRC2.Scenarios.ScenarioFactory
 {
@@ -9,37 +9,103 @@ namespace VRC2.Scenarios.ScenarioFactory
     {
         [Header("Drone")] public GameObject drone;
         public float speed;
-        public GameObject endPosition;
 
         private GameObject player;
+
+        private Vector3 dronePosition;
+
+        private bool moving = false;
+        private bool goBack = false;
+
+        private Timer _timer;
+
+        private Vector3 targetPosition
+        {
+            get
+            {
+                var pos = player.transform.position;
+                pos += player.transform.forward;
+                pos.y += 2.0f; // minimal height
+                return pos;
+            }
+        }
 
         void Start()
         {
             base.Start();
 
             player = localPlayer;
+
+            dronePosition = drone.transform.position;
+
+            moving = false;
+            goBack = false;
+
+            drone.SetActive(false);
+        }
+
+        void StartTimer(int second)
+        {
+            if (_timer != null)
+            {
+                Timer.Cancel(_timer);
+            }
+
+            _timer = Timer.Register(second, () =>
+            {
+                moving = true;
+                goBack = true;
+            }, isLooped: false, useRealTime: true);
+
         }
 
         private void Update()
         {
-            droneMove();
-        }
-        
-        public void droneMove()
-        {
-            drone.transform.position = Vector3.MoveTowards(drone.transform.position, endPosition.transform.position,
-                speed * Time.deltaTime);
+            // droneMove();
+            if (moving)
+            {
+                if (goBack)
+                {
+                    drone.transform.position = Vector3.MoveTowards(drone.transform.position, dronePosition,
+                        speed * Time.deltaTime);
+
+                    if (Vector3.Distance(drone.transform.position, dronePosition) < 0.1f)
+                    {
+                        // reach the destination
+                        moving = false;
+
+                        drone.SetActive(false);
+                    }
+                }
+                else
+                {
+                    drone.transform.position = Vector3.MoveTowards(drone.transform.position, targetPosition,
+                        speed * Time.deltaTime);
+
+                    if (Vector3.Distance(drone.transform.position, targetPosition) < 0.1f)
+                    {
+                        // reach the target
+                        UpdateInstruction();
+
+                        moving = false;
+                        // wait a moment
+                        StartTimer(5);
+                    }
+                }
+            }
         }
 
 
         #region Accident Events Callbacks
 
+        void UpdateInstruction()
+        {
+            Debug.LogWarning("The plan of installment order changed");
+        }
+
         public void On_BaselineS2_1_Start()
         {
             print("On_BaselineS2_1_Start");
-
-            drone.transform.position = Vector3.MoveTowards(drone.transform.position, endPosition.transform.position,
-                speed * Time.deltaTime);
         }
 
         public void On_BaselineS2_1_Finish()
@@ -57,14 +123,16 @@ namespace VRC2.Scenarios.ScenarioFactory
             var warning = incident;
             print(warning);
 
-            drone.transform.position = Vector3.MoveTowards(drone.transform.position, endPosition.transform.position,
-                speed * Time.deltaTime);
+            drone.SetActive(true);
 
+            moving = true;
+            goBack = false;
         }
 
         public void On_BaselineS2_2_Finish()
         {
             // A supervising drone approaches the participants, and informs the second participant of a plan of installment change order. Gives the second participant a new sheet of requirements.
+            moving = false;
         }
 
         public void On_BaselineS2_3_Start()
