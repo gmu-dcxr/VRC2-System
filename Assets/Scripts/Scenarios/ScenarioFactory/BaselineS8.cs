fddeaf8f15b9d149f94a01e8686fc419522acab7
willfredranc <willfredranc@gmail.com>
2023-08-04 14:07:30 -0400

    Added water leak script for BaselineS8 inci 2

--- Assets/Scripts/Scenarios/ScenarioFactory/BaselineS8.cs
+++ Assets/Scripts/Scenarios/ScenarioFactory/BaselineS8.cs
@@ -13,9 +13,14 @@ namespace VRC2.Scenarios.ScenarioFactory
         private Transform Finish1;
 
         public GameObject Truck;
-        private float speed = 1f;
+        public GameObject waterLeak;
+        public GameObject liveWire;
+        private float speed = 6f;
 
         private bool backingUp = false;
+        private bool moveFoward = false;
+        private int trips = 0;
+        private int maxTrips = 5;
 
 
         [Header("Player")] public GameObject player;
@@ -28,14 +33,62 @@ namespace VRC2.Scenarios.ScenarioFactory
             //Find positions
             Start1 = GameObject.Find("Start").transform;
             Finish1 = GameObject.Find("Finish").transform;
+            waterLeak.SetActive(false);
         }
 
         private void Update()
         {
-            if (backingUp)
+            if (trips < maxTrips)
             {
-                Truck.transform.position = Vector3.MoveTowards(Truck.transform.position, Finish1.transform.position,
-                    speed * Time.deltaTime);
+                if (backingUp)
+                {
+                    //back up until each finish, then move foward, waterLeak increases each time    
+                    if (Truck.transform.position.z != (Finish1.position.z))
+                    {
+                        Truck.transform.position = Vector3.MoveTowards(Truck.transform.position, Finish1.transform.position,
+                            speed * Time.deltaTime);
+                    }
+                    if (Truck.transform.position.z == (Finish1.position.z))
+                    {
+                        backingUp = false;
+                        moveFoward = true;
+                        //leakWater(waterLeak);
+                    }
+                }
+
+                if (moveFoward)
+                {
+                    if (Truck.transform.position.z != Start1.position.z)
+                    {
+                        Truck.transform.position = Vector3.MoveTowards(Truck.transform.position, Start1.transform.position,
+                            speed * Time.deltaTime);
+                    }
+                    if (Truck.transform.position.z == (Start1.position.z))
+                    {
+                        moveFoward = false;
+                        backingUp = true;
+                        leakWater(waterLeak);
+                        trips++;
+                    }
+                }
+            }
+        }
+        //runs every time the truck reaches a position (simple)
+        private void leakWater(GameObject waterLeak)
+        {
+            float yValue = waterLeak.transform.localScale.y;
+            Renderer r = waterLeak.GetComponent<Renderer>();
+            Material m = r.material;
+
+            if (!waterLeak.activeSelf) 
+            { 
+                waterLeak.SetActive(true); 
+            }
+            // increase waterLeak size and scale texture
+            else 
+            {
+                waterLeak.transform.localScale += new Vector3(1.0f, 0.0f, 0.0f);
+                m.SetTextureScale("_MainTex", new Vector2(waterLeak.transform.localScale.x, waterLeak.transform.localScale.y));
             }
         }
 
