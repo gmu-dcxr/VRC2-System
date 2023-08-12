f73c95d13bfea9c7549dd308c3b5b1ecacce047e
willfredranc <willfredranc@gmail.com>
2023-08-12 19:01:09 -0400

    fixed merge error

--- Assets/Scripts/Scenarios/ScenarioFactory/BaselineS6.cs
+++ Assets/Scripts/Scenarios/ScenarioFactory/BaselineS6.cs
@@ -39,8 +39,6 @@ namespace VRC2.Scenarios.ScenarioFactory
         {
 
             base.Start();
-
-<<<<<<< HEAD
             player = localPlayer;
 
             scriptTCC = crane.GetComponent<TowerControllerCrane>();
@@ -53,19 +51,8 @@ namespace VRC2.Scenarios.ScenarioFactory
             boomCart.localPosition += new Vector3(boomCartOffset, 0f, 0f);
             boomCartPositionX = boomCart.localPosition.x;
             craneSwivel = scriptTCC.rotationElementCrane.transform;           
-=======
+
             player = localPlayer;
-
-            crane = animator.gameObject;
-            randomYawIncrease = 5; // Random.Range(1, 10);
-
-            triggered = false;
-            backward = false;
-
-            initYaw = CalculateRawBetweenCranePlayer(crane, player);
-            initDolly = dolly;
-
->>>>>>> 6becc7af647e7eb821336d502009163db2213757
 
             unpackedPipe.SetActive(false);
         }
@@ -100,7 +87,6 @@ namespace VRC2.Scenarios.ScenarioFactory
         {
             print("Start Normal Incident Baseline S6");
             
-            TriggerEvent(0, true, false);
         }
 
         public void On_BaselineS6_1_Start()
