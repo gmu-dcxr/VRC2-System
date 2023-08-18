14f9b4cbd0ae071ef1151c8c100d2bfdd22392f4
Liuchuan Yu <lyu20@gmu.edu>
2023-08-18 16:05:32 -0400

    update clamp

---
+++ Assets/Scripts/Hack/ClampNetworkGrabbable.cs
@@ -0,0 +1,33 @@
+﻿using Hack;
+using Oculus.Interaction;
+using UnityEngine;
+using VRC2.Events;
+
+namespace VRC2.Hack
+{
+    public class ClampNetworkGrabbable: NetworkGrabbable
+    {
+        protected override void Start()
+        {
+            this.BeginStart(ref _started, () => base.Start());
+
+            // remove the OneGrabFreeTransformer
+            var ogft = gameObject.GetComponent<OneGrabFreeTransformer>();
+            GameObject.Destroy(ogft);
+
+            // force update the OneGrabTransformer
+            ClampGrabFreeTransformer defaultTransformer = gameObject.AddComponent<ClampGrabFreeTransformer>();
+            _oneGrabTransformer = defaultTransformer;
+            OneGrabTransformer = defaultTransformer;
+            OneGrabTransformer.Initialize(this);
+
+            this.EndStart(ref _started);
+        }
+
+        public override bool NetworkedPointerEvent(PointerEvent evt)
+        {
+            // use the default handler
+            return false;
+        }
+    }
+}
