e04785be2eda082a0ff8c2bc705e2dd8af59e277
Liuchuan Yu <lyu20@gmu.edu>
2023-08-23 15:52:28 -0400

    merge ChingYu's scene

---
+++ Assets/Animation/try/try_move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class try_move : MonoBehaviour
+{
+    public GameObject Sphere;
+    public GameObject Cube;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sphere.transform.position = Vector3.MoveTowards(Sphere.transform.position, Cube.transform.position, speed);    
+    }
+}
