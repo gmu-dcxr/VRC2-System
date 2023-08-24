da60d639b1028fa949110fbb5f3c9fdab95c7e2b
Liuchuan Yu <lyu20@gmu.edu>
2023-08-24 11:43:29 -0400

    add InstructionSheet

---
+++ Assets/Scripts/Events/InstructionSheetGrabbingCallback.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Oculus.Interaction;
+using TMPro;
+using UnityEngine;
+
+namespace VRC2.Events
+{
+    public class InstructionSheetGrabbingCallback : MonoBehaviour
+    {
+        public GameObject dialog;
+        public TextMeshProUGUI titleUI;
+        public TextMeshProUGUI contentUI;
+        public float distance;
+
+        public string title = "Instruction";
+        public string content = "This is instruction";
+
+        private PointableUnityEventWrapper _wrapper;
+
+        private Transform _cameraTransform;
+
+        private void Start()
+        {
+            _cameraTransform = Camera.main.transform;
+
+            _wrapper = gameObject.GetComponent<PointableUnityEventWrapper>();
+
+            _wrapper.WhenSelect.AddListener(OnSelect);
+            _wrapper.WhenRelease.AddListener(OnRelease);
+
+            dialog.SetActive(false);
+        }
+
+        void OnSelect()
+        {
+            print("On select");
+
+            dialog.SetActive(true);
+
+            titleUI.text = title;
+            contentUI.text = content;
+        }
+
+        void OnRelease()
+        {
+            print("On release");
+
+            dialog.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (dialog.activeSelf)
+            {
+                MoveDialogFaceHeadset();
+            }
+        }
+
+        void MoveDialogFaceHeadset()
+        {
+            var forward = _cameraTransform.forward;
+
+            dialog.transform.rotation = _cameraTransform.rotation;
+            dialog.transform.position = _cameraTransform.position + forward * distance;
+        }
+    }
+}
