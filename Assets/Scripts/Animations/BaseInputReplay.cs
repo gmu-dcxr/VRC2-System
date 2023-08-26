703ff93ac1b62d7981ed52af84c26ec37eb129e2
Liuchuan Yu <lyu20@gmu.edu>
2023-08-26 15:07:43 -0400

    add BaseInputReplay and CraneInputReplay

---
+++ Assets/Scripts/Animations/BaseInputReplay.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+namespace VRC2.Animations
+{
+    public class BaseInputReplay : MonoBehaviour
+    {
+        private string folder = "InputRecordings";
+
+        string GetFilePath(string filename) =>
+            Path.Combine(Path.GetDirectoryName(Application.dataPath), folder, filename + ".inputtrace");
+
+        public virtual void Awake()
+        {
+            InitTraces();
+            InitControllers();
+        }
+
+        public virtual void InitTraces()
+        {
+
+        }
+
+        public virtual void InitControllers()
+        {
+            
+        }
+
+        public void InitTrace(ref InputEventTrace trace, string filename)
+        {
+            if (trace == null)
+            {
+                trace = new InputEventTrace(Keyboard.current);
+                trace.onEvent += OnEvent;
+                trace.ReadFrom(GetFilePath(filename));
+            }
+        }
+
+        void OnEvent(InputEventPtr ev)
+        {
+            Debug.Log(ev.ToString());
+        }
+
+        // public void Replay(InputEventTrace trace, Action finish)
+        // {
+        //     trace.Replay()
+        //         .OnFinished(finish)
+        //         .PlayAllEventsAccordingToTimestamps();
+        // }
+
+        public InputEventTrace.ReplayController InitController(ref InputEventTrace trace, Action onfinished, Action<InputEventPtr> action)
+        {
+            return trace.Replay()
+                .OnFinished(onfinished)
+                .OnEvent(action);
+        }
+
+        public void StartReplay(ref InputEventTrace.ReplayController controller)
+        {
+            controller.PlayAllEventsAccordingToTimestamps();
+        }
+    }
+}
---
+++ Assets/Scripts/Animations/CraneInputReplay.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+namespace VRC2.Animations
+{
+    public class CraneInputReplay : BaseInputReplay
+    {
+        [Header("Markers")] public Transform pickupTransform;
+        public Transform dropoffTransform;
+
+        [Space(30)] [Header("Filename")] public string forwardFile;
+        public string backwardFile;
+        public string pickupFile;
+        public string dropoffFile;
+        public string leftFile;
+        public string rightFile;
+
+        #region Traces
+
+        private InputEventTrace forwardET;
+        private InputEventTrace backwardET;
+        private InputEventTrace pickupET;
+        private InputEventTrace dropoffET;
+        private InputEventTrace leftET;
+        private InputEventTrace rightET;
+
+        #endregion
+
+        #region Controllers
+
+        private InputEventTrace.ReplayController forwardController;
+        private InputEventTrace.ReplayController backwardController;
+        private InputEventTrace.ReplayController pickupController;
+        private InputEventTrace.ReplayController dropoffController;
+        private InputEventTrace.ReplayController leftController;
+        private InputEventTrace.ReplayController rightController;
+
+
+
+        #endregion
+
+        #region Override functions
+
+        public override void InitTraces()
+        {
+            print("InitTraces");
+            InitTrace(ref forwardET, forwardFile);
+            InitTrace(ref backwardET, backwardFile);
+            InitTrace(ref pickupET, pickupFile);
+            InitTrace(ref dropoffET, dropoffFile);
+            InitTrace(ref leftET, leftFile);
+            InitTrace(ref rightET, rightFile);
+        }
+
+        public override void InitControllers()
+        {
+            print("InitControllers");
+            forwardController = InitController(ref forwardET, OnFinished, OnEvent);
+            backwardController = InitController(ref backwardET, OnFinished, OnEvent);
+            pickupController = InitController(ref pickupET, OnFinished, OnEvent);
+            dropoffController = InitController(ref dropoffET, OnFinished, OnEvent);
+            leftController = InitController(ref leftET, OnFinished, OnEvent);
+            rightController = InitController(ref rightET, OnFinished, OnEvent);
+        }
+
+        #endregion
+
+        private void Start()
+        {
+            
+        }
+
+        #region Controllers' callbacks
+
+        private void OnEvent(InputEventPtr obj)
+        {
+            print("onevent");
+        }
+
+        void OnFinished()
+        {
+
+        }
+
+        #endregion
+
+        #region Debug
+
+        private void OnGUI()
+        {
+            if (GUI.Button(new Rect(10, 10, 100, 50), "Start"))
+            {
+                StartReplay(ref forwardController);
+            }
+        }
+
+        #endregion
+    }
+}
