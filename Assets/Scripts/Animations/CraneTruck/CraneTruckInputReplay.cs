fd627706b357782f2467a83fee101ff1d64f953d
Liuchuan Yu <yuliuchuan@gmail.com>
2023-08-29 20:39:18 -0400

    add crane truck input replay

---
+++ Assets/Scripts/Animations/CraneTruck/CraneTruckInputReplay.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+namespace VRC2.Animations.CraneTruck
+{
+    public class CraneTruckInputReplay : BaseInputReplay
+    {
+        [Space(30)] [Header("Filename")] public string forwardFile;
+        public string backwardFile;
+        public string leftFile;
+        public string rightFile;
+        public string stopFile;
+        public string dropoffFile;
+
+        #region Traces
+
+        private InputEventTrace forwardET;
+        private InputEventTrace backwardET;
+        private InputEventTrace leftET;
+        private InputEventTrace rightET;
+        private InputEventTrace stopET;
+        private InputEventTrace dropoffET;
+
+        #endregion
+
+        #region Controllers
+
+        private InputEventTrace.ReplayController forwardController;
+        private InputEventTrace.ReplayController backwardController;
+        private InputEventTrace.ReplayController leftController;
+        private InputEventTrace.ReplayController rightController;
+        private InputEventTrace.ReplayController stopController;
+        private InputEventTrace.ReplayController dropoffController;
+
+
+
+        #endregion
+
+        #region Override functions
+
+        public override void InitTraces()
+        {
+            print("InitTraces");
+            InitTrace(ref forwardET, forwardFile);
+            InitTrace(ref backwardET, backwardFile);
+            InitTrace(ref leftET, leftFile);
+            InitTrace(ref rightET, rightFile);
+            InitTrace(ref stopET, stopFile);
+            InitTrace(ref dropoffET, dropoffFile);
+        }
+
+        public override void InitControllers()
+        {
+            print("InitControllers");
+            forwardController = InitController(ref forwardET, OnFinished, OnEvent);
+            backwardController = InitController(ref backwardET, OnFinished, OnEvent);
+            leftController = InitController(ref leftET, OnFinished, OnEvent);
+            rightController = InitController(ref rightET, OnFinished, OnEvent);
+            stopController = InitController(ref stopET, OnFinished, OnEvent);
+            dropoffController = InitController(ref dropoffET, OnFinished, OnEvent);
+        }
+
+        #endregion
+
+        #region Controllers' callbacks
+
+        private void OnEvent(InputEventPtr obj)
+        {
+
+        }
+
+        void OnFinished()
+        {
+
+        }
+
+        #endregion
+    }
+}
