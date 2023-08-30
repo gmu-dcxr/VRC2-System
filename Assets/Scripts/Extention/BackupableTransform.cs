fe038806983e9bea686395122288a2c1c099744f
Liuchuan Yu <yuliuchuan@gmail.com>
2023-08-29 22:14:03 -0400

    finish integrating recording/replay in S5

---
+++ Assets/Scripts/Extention/BackupableTransform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace VRC2.Extention
+{
+    public class BackupableTransform
+    {
+        public Vector3 position { get; set; }
+
+        public Quaternion rotation { get; set; }
+
+        public BackupableTransform()
+        {
+            position = Vector3.zero;
+            rotation = Quaternion.identity;
+        }
+
+        public void Restore(ref GameObject go)
+        {
+            go.transform.position = this.position;
+            go.transform.rotation = this.rotation;
+        }
+
+        public static BackupableTransform Clone(GameObject go)
+        {
+            var t = go.transform;
+            BackupableTransform bt = new BackupableTransform();
+            bt.position = t.position;
+            bt.rotation = t.rotation;
+
+            return bt;
+        }
+    }
+}
