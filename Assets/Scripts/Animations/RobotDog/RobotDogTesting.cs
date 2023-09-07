418961ab2696d3d8d2b70084d9730055a2fce8a7
Liuchuan Yu <yuliuchuan@gmail.com>
2023-09-06 21:15:18 -0400

    WIP: integrating the robotdog

--- Assets/Scripts/Animations/RobotDog/RobotDogTesting.cs
+++ Assets/Scripts/Animations/RobotDog/RobotDogTesting.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 using UnityEngine;
 
+using System;
+using Fusion;
+using Oculus.Interaction.DistanceReticles;
+using UnityEngine;
+using UnityEngine.AI;
+using VRC2.Events;
+using VRC2.Pipe;
+using PipeBendAngles = VRC2.Pipe.PipeConstants.PipeBendAngles;
+using AgentHelper = VRC2.Agent.AgentHelper;
+using PipeParameters = VRC2.Pipe.PipeConstants.PipeParameters;
+using PipeColor = VRC2.Pipe.PipeConstants.PipeColor;
+using PipeType = VRC2.Pipe.PipeConstants.PipeType;
+
 namespace VRC2.Animations
 {
     internal enum RobotStage
@@ -373,5 +386,260 @@ namespace VRC2.Animations
                 replay.Pickup();
             }
         }
+
+        #region WIP - Adaptation
+        
+        [Header("Robot Setting")] public GameObject robot;
+        public Transform robotBase;
+        public Transform robotHand;
+
+        [Header("Grab Offset")] public Vector3 positionOffset = Vector3.zero;
+        public Vector3 rotationOffset = Vector3.zero;
+
+        private GameObject currentPipe
+        {
+            get => GlobalConstants.lastSpawnedPipe;
+        }
+
+        private PipeParameters parameters;
+
+        private NetworkObject spawnedPipe;
+
+        private NavMeshAgent _agent;
+
+        private RobotRoutine _routine;
+
+        private Vector3 destination;
+
+        public System.Action<PipeBendAngles> ReadyToOperate;
+        
+         void UpdateLocalSpawnedPipe(GameObject go)
+        {
+            var pm = go.GetComponent<PipeManipulation>();
+
+            // set material
+            pm.SetMaterial(parameters);
+            // set length
+            pm.SetLength(parameters.a, parameters.b);
+        }
+
+        // update spawned pipe since it might be different from the prefab
+        void UpdateRemoteSpawnedPipe(NetworkId nid, PipeType type, PipeColor color, float a,
+            float b)
+        {
+            var runner = GlobalConstants.networkRunner;
+            var go = runner.FindObject(nid).gameObject;
+
+            // update material
+            var pm = go.GetComponent<PipeManipulation>();
+
+            // set material
+            pm.SetMaterial(type, color);
+            // set length
+            pm.SetLength(a, b);
+        }
+
+        void SpawnPipeUsingSelected()
+        {
+            // unparent
+            currentPipe.transform.parent = null;
+
+            var t = currentPipe.transform;
+            var pos = t.position;
+            var rot = t.rotation;
+            // var scale = t.localScale;
+
+            // destroy
+            GameObject.DestroyImmediate(currentPipe);
+            GlobalConstants.lastSpawnedPipe = null;
+
+
+            // spawn object
+            var runner = GlobalConstants.networkRunner;
+            var localPlayer = GlobalConstants.localPlayer;
+
+            var prefab = PipeHelper.GetPipePrefabRef(parameters);
+
+            spawnedPipe = runner.Spawn(prefab, pos, rot, localPlayer);
+        }
+
+        [Rpc(RpcSources.All, RpcTargets.All)]
+        private void RPC_SendMessage(NetworkId nid, PipeType type, PipeColor color, float a,
+            float b,
+            RpcInfo info = default)
+        {
+            var message = "";
+
+            if (info.IsInvokeLocal)
+            {
+                message = $"RobotBendCut message ({type}, {color}, {a}, {b})\n";
+                Debug.LogWarning(message);
+            }
+            else
+            {
+                message = $"RobotBendCut received message ({type}, {color}, {a}, {b})\n";
+                Debug.LogWarning(message);
+
+                // update spawned object material
+                UpdateRemoteSpawnedPipe(nid, type, color, a, b);
+            }
+        }
+
+
+        // private void Start()
+        // {
+        //     _routine = RobotRoutine.Default;
+        //     _agent = robot.GetComponent<NavMeshAgent>();
+        //     _agent.stoppingDistance = 0.5f;
+        // }
+
+        // private void Update()
+        // {
+        //     switch (_routine)
+        //     {
+        //         case RobotRoutine.PickUp:
+        //             PickupHandler();
+        //             break;
+        //         case RobotRoutine.BendCut:
+        //             BendCutHandler();
+        //             break;
+        //         case RobotRoutine.GetResult:
+        //             GetResultHandler();
+        //             break;
+        //         case RobotRoutine.Waiting:
+        //             break;
+        //
+        //         case RobotRoutine.DropOff:
+        //             DropoffHandler();
+        //             break;
+        //     }
+        // }
+
+        public void InitParameters(PipeBendAngles angle, float a, float b)
+        {
+            parameters.angle = angle;
+            parameters.a = a;
+            parameters.b = b;
+            // color and type and from global constants
+            // get color and type
+            var pm = currentPipe.GetComponent<PipeManipulation>();
+            parameters.color = pm.pipeColor;
+            parameters.type = pm.pipeType;
+            parameters.diameter = pm.diameter;
+        }
+
+        public void PickUp()
+        {
+            Debug.Log("Robot PickUp");
+            // get the pipe from P1, and move to pickup the pipe
+            
+            targetTransform = currentPipe.transform;
+            
+            MoveToTarget();
+        }
+
+        public void Execute()
+        {
+            PickUp();
+        }
+
+        public NetworkObject SpawnPipe(PipeBendAngles angle)
+        {
+            // update angle
+            parameters.angle = angle;
+            
+            // start bend/cut
+            SpawnPipeUsingSelected();
+            UpdateLocalSpawnedPipe(spawnedPipe.gameObject);
+            // update remote object
+            RPC_SendMessage(spawnedPipe.Id, parameters.type, parameters.color, parameters.a, parameters.b);
+            return spawnedPipe;
+        }
+
+        public void PickupResult(Vector3 des)
+        {
+            // move to pick result
+            _routine = RobotRoutine.GetResult;
+            _agent.SetDestination(des);
+        }
+
+        #region Handlers
+
+        void PickupHandler()
+        {
+            // reach to the workspace
+            if (AgentHelper.ReachDestination(_agent))
+            {
+                // save for future delivery
+                destination = currentPipe.transform.position;
+                // set pipe parent to robot hand
+                currentPipe.transform.parent = robotHand;
+                // update local position and rotation
+                currentPipe.transform.localPosition = positionOffset;
+                currentPipe.transform.localRotation = Quaternion.Euler(rotationOffset);
+
+                var go = currentPipe;
+
+                PipeHelper.BeforeMove(ref go);
+
+                // move to robot base
+                _routine = RobotRoutine.BendCut;
+                _agent.SetDestination(robotBase.position);
+            }
+        }
+
+        // TODO: apply when reaching the machine
+        void BendCutHandler()
+        {
+            if (AgentHelper.ReachDestination(_agent))
+            {
+                if (ReadyToOperate != null)
+                {
+                    ReadyToOperate(parameters.angle);
+                }
+
+                _routine = RobotRoutine.Waiting;
+            }
+        }
+
+        // TODO: when the result carried by the robot dog reaches the delivery point
+        void DropoffHandler()
+        {
+            if (AgentHelper.ReachDestination(_agent))
+            {
+                spawnedPipe.transform.parent = null;
+
+                var go = spawnedPipe.gameObject;
+                PipeHelper.AfterMove(ref go);
+
+                // return to base
+                _routine = RobotRoutine.Default;
+                _agent.SetDestination(robotBase.position);
+            }
+        }
+
+        // TODO: when the result is available
+        void GetResultHandler()
+        {
+            if (AgentHelper.ReachDestination(_agent))
+            {
+                var go = spawnedPipe.gameObject;
+                PipeHelper.BeforeMove(ref go);
+
+                // parent object to robot hand
+                spawnedPipe.transform.parent = robotHand;
+
+                spawnedPipe.transform.localPosition = positionOffset;
+                spawnedPipe.transform.localRotation = Quaternion.Euler(rotationOffset);
+
+                // move to workspace
+                _routine = RobotRoutine.DropOff;
+                _agent.SetDestination(destination);
+            }
+        }
+
+        #endregion
+
+        #endregion
     }
 }
