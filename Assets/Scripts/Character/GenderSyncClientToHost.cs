522ff5c40f246a30c264b1031594177a14aae764
Liuchuan Yu <lyu20@gmu.edu>
2023-09-20 18:07:29 -0400

    split host 2 client and client 2 host

---
+++ Assets/Scripts/Character/GenderSyncClientToHost.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Fusion;
+using UnityEngine;
+
+namespace VRC2.Character
+{
+    public class GenderSyncClientToHost : NetworkBehaviour
+    {
+        private PlayerSpawner _playerSpawner;
+
+        private bool synchronized = false;
+
+        private GameObject selfGameObject
+        {
+            get => _playerSpawner.GetSelfObject();
+        }
+
+        private void Start()
+        {
+            _playerSpawner = FindObjectOfType<PlayerSpawner>();
+            synchronized = false;
+        }
+
+        private void Update()
+        {
+            if (synchronized) return;
+
+            if (_playerSpawner.ReadyToSyncGender() && Runner.IsClient)
+            {
+                var pid = GlobalConstants.localPlayer.PlayerId;
+                var male = GlobalConstants.playerGender == PlayerGender.Male;
+                // send message
+                RPC_SendMessage(pid, male);
+                synchronized = true;
+            }
+        }
+
+        PlayerRef GetPlayerByPID(int pid)
+        {
+            var players = Runner.ActivePlayers;
+            foreach (var p in players)
+            {
+                if (p.PlayerId == pid) return p;
+            }
+
+            return PlayerRef.None;
+        }
+
+        [Rpc(RpcSources.All, RpcTargets.All)]
+        private void RPC_SendMessage(int playerid, bool male, RpcInfo info = default)
+        {
+            if (info.IsInvokeLocal)
+            {
+                print("GenderSyncClientToHost local invoke");
+            }
+            else
+            {
+                print("GenderSyncClientToHost remote invoke");
+            }
+
+            print($"GenderSyncClientToHost GenderSyncer: {playerid} {male}");
+            // find gameobject by player id
+            var pr = GetPlayerByPID(playerid);
+            var go = Runner.GetPlayerObject(pr);
+
+            print($"GenderSyncClientToHost network object id: {go.Id}");
+
+            // get GenderSelector
+            var gs = go.GetComponent<GenderSelector>();
+
+            if (male)
+            {
+                gs.ChangeToMale();
+            }
+            else
+            {
+                gs.ChangeToFemale();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Character/GenderSyncHostToClient.cs
@@ -0,0 +1,80 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace VRC2.Character
+{
+    public class GenderSyncHostToClient : NetworkBehaviour
+    {
+        private PlayerSpawner _playerSpawner;
+
+        private bool synchronized = false;
+
+        private GameObject selfGameObject
+        {
+            get => _playerSpawner.GetSelfObject();
+        }
+
+        private void Start()
+        {
+            _playerSpawner = FindObjectOfType<PlayerSpawner>();
+            synchronized = false;
+        }
+
+        private void Update()
+        {
+            if (synchronized) return;
+
+            if (_playerSpawner.ReadyToSyncGender() && Runner.IsServer)
+            {
+                var pid = GlobalConstants.localPlayer.PlayerId;
+                var male = GlobalConstants.playerGender == PlayerGender.Male;
+                // send message
+                RPC_SendMessage(pid, male);
+                synchronized = true;
+            }
+        }
+
+        PlayerRef GetPlayerByPID(int pid)
+        {
+            var players = Runner.ActivePlayers;
+            foreach (var p in players)
+            {
+                if (p.PlayerId == pid) return p;
+            }
+
+            return PlayerRef.None;
+        }
+
+        [Rpc(RpcSources.All, RpcTargets.All)]
+        private void RPC_SendMessage(int playerid, bool male, RpcInfo info = default)
+        {
+            if (info.IsInvokeLocal)
+            {
+                print("GenderSyncHostToClient local invoke");
+            }
+            else
+            {
+                print("GenderSyncHostToClient remote invoke");
+            }
+
+            print($"GenderSyncHostToClient GenderSyncer: {playerid} {male}");
+            // find gameobject by player id
+            var pr = GetPlayerByPID(playerid);
+            var go = Runner.GetPlayerObject(pr);
+
+            print($"GenderSyncHostToClient network object id: {go.Id}");
+
+            // get GenderSelector
+            var gs = go.GetComponent<GenderSelector>();
+
+            if (male)
+            {
+                gs.ChangeToMale();
+            }
+            else
+            {
+                gs.ChangeToFemale();
+            }
+        }
+    }
+}
