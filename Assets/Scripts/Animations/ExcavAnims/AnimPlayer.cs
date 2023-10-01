c9803b20b1e45301a1de0cdbe35bae6d2ee0aabc
michaelszilagyi <80373779+michaelszilagyi@users.noreply.github.com>
2023-10-01 12:10:19 -0400

    Partial integration of the new excav animation system

---
+++ Assets/Scripts/Animations/ExcavAnims/AnimPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPlayer : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("Left", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
