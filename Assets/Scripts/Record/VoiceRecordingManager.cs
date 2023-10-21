062ee77686e77292d62c73e61a2cbe52e2c08f02
Liuchuan Yu <lyu20@gmu.edu>
2023-10-21 16:20:38 -0400

    add VoiceClientMonitor and VoiceRecordingManager

---
+++ Assets/Scripts/Record/VoiceRecordingManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace VRC2.Record
+{
+    public class VoiceRecordingManager : MonoBehaviour
+    {
+        public VoiceClientMonitor monitor;
+
+
+        private void Start()
+        {
+            monitor.OnVoiceLinkDetermined += OnVoiceLinkDetermined;
+        }
+
+        private void OnVoiceLinkDetermined()
+        {
+            print("OnVoiceLinkDetermined");
+        }
+    }
+}
