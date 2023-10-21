1f4efb3fa24df5a9300343317d76816e311b5d1d
Liuchuan Yu <lyu20@gmu.edu>
2023-10-21 18:06:12 -0400

    add save local/remote voice stream

---
+++ Assets/Scripts/Record/SaveRemoteVoiceStream.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.IO;
+using Photon.Voice;
+using Photon.Voice.Unity;
+using Photon.Voice.Unity.UtilityScripts;
+
+namespace VRC2.Record
+{
+    using UnityEngine;
+    using System.IO;
+
+    [RequireComponent(typeof(VoiceConnection))]
+    [DisallowMultipleComponent]
+    public class SaveRemoteVoiceStream : VoiceComponent
+    {
+        private VoiceConnection voiceConnection;
+
+        [SerializeField] private bool muteLocalSpeaker = false;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            this.voiceConnection = this.GetComponent<VoiceConnection>();
+            this.voiceConnection.RemoteVoiceAdded += this.OnRemoteVoiceAdded;
+            this.voiceConnection.SpeakerLinked += this.OnSpeakerLinked;
+        }
+
+        private void OnSpeakerLinked(Speaker speaker)
+        {
+            if (this.muteLocalSpeaker && speaker.RemoteVoice.PlayerId == voiceConnection.Client.LocalPlayer.ActorNumber)
+            {
+                AudioSource audioSource = speaker.GetComponent<AudioSource>();
+                audioSource.mute = true;
+                audioSource.volume = 0f;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            this.voiceConnection.RemoteVoiceAdded -= this.OnRemoteVoiceAdded;
+        }
+
+        private void OnRemoteVoiceAdded(RemoteVoiceLink remoteVoiceLink)
+        {
+            int bitsPerSample = 32;
+            string filePath = this.GetFilePath(remoteVoiceLink);
+            Debug.Log($"Write voice recording to: {filePath}");
+            this.Logger.LogInfo("Incoming stream {0}, output file path: {1}", remoteVoiceLink.VoiceInfo, filePath);
+            WaveWriter waveWriter = new WaveWriter(filePath, remoteVoiceLink.VoiceInfo.SamplingRate, bitsPerSample,
+                remoteVoiceLink.VoiceInfo.Channels);
+            remoteVoiceLink.FloatFrameDecoded += f => { waveWriter.WriteSamples(f.Buf, 0, f.Buf.Length); };
+            remoteVoiceLink.RemoteVoiceRemoved += () =>
+            {
+                this.Logger.LogInfo("Remote voice stream removed: Saving wav file.");
+                waveWriter.Dispose();
+            };
+        }
+
+        private string GetFilePath(RemoteVoiceLink remoteVoiceLink)
+        {
+            string filename = string.Format("in_{0}_{1}_{2}_{3}_{4}.wav",
+                System.DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss-ffff"), Random.Range(0, 1000),
+                remoteVoiceLink.ChannelId, remoteVoiceLink.PlayerId, remoteVoiceLink.VoiceId);
+
+            return Path.Combine(Path.GetDirectoryName(Application.dataPath), "VoiceRecordings", filename);
+            // return Path.Combine(Application.persistentDataPath, filename);
+        }
+    }
+}
+
