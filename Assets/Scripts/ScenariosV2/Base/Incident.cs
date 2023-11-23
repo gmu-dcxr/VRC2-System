4614d44a4d5d1bdfd94de3090a2da1c928c97580
Liuchuan Yu <lyu20@gmu.edu>
2023-11-23 14:35:46 -0500

    add ScenariosV2/

---
+++ Assets/Scripts/ScenariosV2/Base/Incident.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace VRC2.ScenariosV2.Base
+{
+    public class Incident: MonoBehaviour
+    {
+        #region Attributes
+
+        private int _id;
+        private string _name;
+        private string _desc;
+        private string _warning;
+        private string _wfile; // warning sound file
+        private int _stime;
+        private int _duration;
+        private int _endtime;
+
+        #endregion
+
+        #region Methods
+
+        public virtual void InitializeIncident()
+        {
+            
+        }
+
+        public virtual void RunIncident()
+        {
+            
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/ScenariosV2/Base/YamlParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace VRC2.ScenariosV2.Base
+{
+    public class YamlParser
+    {
+        public class Incident
+        {
+            public int id { get; set; }
+            public string condition { get; set; }
+            public string time { get; set; }
+            [CanBeNull] public string type { get; set; }
+            public string desc { get; set; }
+            [CanBeNull] public string warning { get; set; }
+        }
+
+        public class Incidents
+        {
+            [CanBeNull] public List<Incident> normals { get; set; }
+            [CanBeNull] public List<Incident> accidents { get; set; }
+        }
+
+        public class Vehicle
+        {
+            public string name { get; set; }
+            public string desc { get; set; }
+            public List<string> variables { get; set; }
+            public string gameObject { get; set; }
+            public Incidents incidents { get; set; }
+        }
+    }
+}
