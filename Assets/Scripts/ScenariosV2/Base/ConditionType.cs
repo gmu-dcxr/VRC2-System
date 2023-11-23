1e195006989c6aa0a5fa6be33865bfbdda10aa46
Liuchuan Yu <lyu20@gmu.edu>
2023-11-23 14:35:46 -0500

    add ScenariosV2/

---
+++ Assets/Scripts/ScenariosV2/Base/ConditionType.cs
@@ -0,0 +1,14 @@
+﻿namespace VRC2.ScenariosV2.Base
+{
+    public enum ConditionType
+    {
+        Normal,
+        Accident,
+    }
+
+    public enum AccidentType
+    {
+        Potential,
+        NearlyHappen
+    }
+}
---
+++ Assets/Scripts/ScenariosV2/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Helper = VRC2.Scenarios.Helper;
+using VRC2.ScenariosV2.Base;
+using YamlDotNet.Serialization;
+
+namespace VRC2.ScenariosV2
+{
+    public class UnitTest: MonoBehaviour
+    {
+        private void Start()
+        {
+            var name = "Crane.yml";
+            var path = Helper.GetConfigureFile(Application.dataPath, name);
+            print(path);
+
+            var text = System.IO.File.ReadAllText(path);
+            print(text);
+
+            var deser = new DeserializerBuilder().Build();
+            var Vehicle = deser.Deserialize<YamlParser.Vehicle>(text);
+            print(Vehicle);
+        }
+    }
+}
