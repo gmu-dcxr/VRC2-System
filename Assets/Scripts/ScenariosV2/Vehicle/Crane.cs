17e747b569df7f1690f064bb6c30b61352d5ce26
Liuchuan Yu <lyu20@gmu.edu>
2023-11-27 14:10:16 -0500

    update Crane.cs

--- Assets/Scripts/ScenariosV2/Vehicle/Crane.cs
+++ Assets/Scripts/ScenariosV2/Vehicle/Crane.cs
@@ -45,6 +45,65 @@ namespace VRC2.ScenariosV2.Vehicle
         public void Crane_accidents_2()
         {
             print("Invoked Crane_accidents_2");
+
+            baselineS1.ShowWarning(3);
+            baselineS1.On_BaselineS1_3_Start();
+        }
+
+        public void Crane_accidents_3()
+        {
+            print("Invoked Crane_accidents_3");
+
+            baselineS1.ShowWarning(4);
+            baselineS1.On_BaselineS1_4_Start();
+        }
+
+        public void Crane_accidents_4()
+        {
+            print("Invoked Crane_accidents_4");
+
+            baselineS1.ShowWarning(5);
+            baselineS1.On_BaselineS1_5_Start();
+        }
+
+        public void Crane_accidents_5()
+        {
+            print("Invoked Crane_accidents_5");
+
+            baselineS1.ShowWarning(6);
+            baselineS1.On_BaselineS1_6_Start();
+        }
+
+        public void Crane_accidents_6()
+        {
+            print("Invoked Crane_accidents_6");
+
+            baselineS1.ShowWarning(7);
+            baselineS1.On_BaselineS1_7_Start();
+        }
+
+        public void Crane_accidents_7()
+        {
+            print("Invoked Crane_accidents_7");
+
+            baselineS1.ShowWarning(8);
+            baselineS1.On_BaselineS1_8_Start();
+        }
+
+        public void Crane_accidents_8()
+        {
+            print("Invoked Crane_accidents_8");
+
+            baselineS1.ShowWarning(9);
+            baselineS1.On_BaselineS1_9_Start();
+        }
+
+        public void Crane_accidents_9()
+        {
+            print("Invoked Crane_accidents_9");
+
+            baselineS1.ShowWarning(10);
+            baselineS1.On_BaselineS1_10_Start();
         }
 
         #endregion
