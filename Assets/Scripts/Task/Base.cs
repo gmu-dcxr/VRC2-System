7c95f563de9d20fb30851a7ea3578d54a66c13d3
Liuchuan Yu <lyu20@gmu.edu>
2024-01-28 12:52:41 -0500

    update task

---
+++ Assets/Scripts/Task/Base.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+using VRC2.Utility;
+
+namespace VRC2.Task
+{
+    public class Base : MonoBehaviour
+    {
+        [Header("Filename")] [Tooltip("Filename under Assets/Conf, e.g. Task/Training.yml")]
+        public string filename;
+
+
+        private void Start()
+        {
+            ParseYmlFile();
+        }
+
+        public void ParseYmlFile()
+        {
+            var path = Helper.GetConfigureFile(Application.dataPath, filename);
+
+            var task = Helper.ParseYamlFile<YamlParser.Task>(path);
+            print(task);
+        }
+    }
+}
