f102f6b1d1bad682ab045d80995229bd973e1bcd
michaelszilagyi <80373779+michaelszilagyi@users.noreply.github.com>
2024-01-29 11:24:29 -0500

    New Michael scene with Scenario3

---
+++ Assets/Scripts/ScenariosV2/Vehicle/Truck.cs
@@ -0,0 +1,51 @@
+﻿namespace VRC2.ScenariosV2.Vehicle
+{
+    public class Truck : Base
+    {
+        #region Callbacks
+
+        public void Truck_normals_1()
+        {
+            print("Invoked Truck_normals_1");
+        }
+
+        public void Truck_normals_2()
+        {
+
+        }
+
+        public void Truck_accidents_1()
+        {
+        }
+
+        public void Truck_accidents_2()
+        {
+        }
+
+        public void Truck_accidents_3()
+        {
+        }
+
+        public void Truck_accidents_4()
+        {
+        }
+
+        public void Truck_accidents_5()
+        {
+        }
+
+        public void Truck_accidents_6()
+        {
+        }
+
+        public void Truck_accidents_7()
+        {
+        }
+
+        public void Truck_accidents_8()
+        {
+        }
+
+        #endregion
+    }
+}
