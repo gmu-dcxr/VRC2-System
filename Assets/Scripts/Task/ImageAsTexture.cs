e0011bf3061c73bb6f1eb6e38ae159444bd19d8d
Liuchuan Yu <lyu20@gmu.edu>
2024-01-29 11:40:40 -0500

    add SheetsTables and ImageAsTexture

---
+++ Assets/Scripts/Task/ImageAsTexture.cs
@@ -0,0 +1,58 @@
+﻿// this class is almost the same as QuadImageManager except fields are not ReadOnly.
+using UnityEngine;
+
+namespace VRC2.Task
+{
+    public class ImageAsTexture : MonoBehaviour
+    {
+        [Header("Image")] [Tooltip("Folder name under the Assets/Resource folder")]
+        public string folder;
+
+        [Tooltip("No filetype extension, e.g., using `image` not `image.png`")]
+        public string filename;
+
+        private Texture2D _texture;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetImageAsTexture();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void SetImageAsTexture()
+        {
+            var name = $"{folder}/{filename}";
+            print($"set image texture: {name}");
+            _texture = Resources.Load<Texture2D>(name);
+            _texture.alphaIsTransparency = true;
+            gameObject.GetComponent<MeshRenderer>().materials[0].mainTexture = _texture;
+
+            SetTexture(_texture);
+
+        }
+
+        public void SetTexture(Texture2D texture2D)
+        {
+            gameObject.GetComponent<MeshRenderer>().materials[0].mainTexture = texture2D;
+        }
+
+        public void UpdateFolderFilename(string folder, string filename)
+        {
+            this.folder = folder;
+            this.filename = filename;
+        }
+
+
+        public void UpdateFilename(string fn)
+        {
+            filename = fn;
+            SetImageAsTexture();
+        }
+    }
+}
