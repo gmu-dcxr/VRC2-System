57e87c6589f58563906488c982977f8230cc3629
Liuchuan Yu <lyu20@gmu.edu>
2024-02-03 16:38:53 -0500

    update scenario 4 design

---
+++ Assets/Scripts/ScenariosV2/Vehicle/Irrelevant.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using VRC2.Scenarios.ScenarioFactory;
+using VRC2.ScenariosV2.Tool;
+
+namespace VRC2.ScenariosV2.Vehicle
+{
+    public class Irrelevant : Base
+    {
+        #region Callbacks
+
+        public void Irrelevant_accidents_1()
+        {
+
+        }
+
+        public void Irrelevant_accidents_2()
+        {
+
+        }
+
+        public void Irrelevant_accidents_3()
+        {
+
+        }
+
+        public void Irrelevant_accidents_4()
+        {
+
+        }
+
+        public void Irrelevant_accidents_5()
+        {
+
+        }
+
+        public void Irrelevant_accidents_6()
+        {
+
+        }
+
+        public void Irrelevant_accidents_7()
+        {
+
+        }
+
+        public void Irrelevant_accidents_8()
+        {
+
+        }
+
+        public void Irrelevant_accidents_9()
+        {
+
+        }
+
+        public void Irrelevant_accidents_10()
+        {
+
+        }
+
+        #endregion
+    }
+}
