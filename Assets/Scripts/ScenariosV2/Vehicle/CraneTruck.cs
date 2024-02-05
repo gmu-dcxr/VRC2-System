8130b46eac38ddcd92d95e8e207892a887758e16
willfredranc <willfredranc@gmail.com>
2024-02-05 15:50:07 -0500

    Added to Drone Callbackcs in Drone.cs

---
+++ Assets/Scripts/ScenariosV2/Vehicle/CraneTruck.cs
@@ -0,0 +1,63 @@
+﻿namespace VRC2.ScenariosV2.Vehicle
+{
+    public class CraneTruck : Base
+    {
+        #region Callbacks
+
+        public void CraneTruck_normals_1()
+        {
+            print("Invoked CraneTruck_normals_1");
+        }
+
+        public void CraneTruck_normals_2()
+        {
+
+        }
+
+        public void CraneTruck_normals_3() 
+        {
+            
+        } 
+        
+        public void CraneTruck_normals_4() 
+        {
+            
+        }
+
+
+
+        public void CraneTruck_accidents_1()
+        {
+        }
+
+        public void CraneTruck_accidents_2()
+        {
+        }
+
+        public void CraneTruck_accidents_3()
+        {
+        }
+
+        public void CraneTruck_accidents_4()
+        {
+        }
+
+        public void CraneTruck_accidents_5()
+        {
+        }
+
+        public void CraneTruck_accidents_6()
+        {
+        }
+
+        public void CraneTruck_accidents_7()
+        {
+        }
+
+        public void CraneTruck_accidents_8()
+        {
+        }
+
+        #endregion
+    }
+}
