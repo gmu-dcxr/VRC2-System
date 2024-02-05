b3d039f3f775c9e2a3496d9b0c1a0b30955002f7
willfredranc <willfredranc@gmail.com>
2024-02-05 15:50:07 -0500

    Added to Drone Callbackcs in Drone.cs

--- Assets/Scripts/ScenariosV2/Vehicle/Drone.cs
+++ Assets/Scripts/ScenariosV2/Vehicle/Drone.cs
@@ -1,17 +1,71 @@
-﻿namespace VRC2.ScenariosV2.Vehicle
+﻿using System;
+using UnityEngine;
+using VRC2.Scenarios.ScenarioFactory;
+using VRC2.ScenariosV2.Tool;
+
+namespace VRC2.ScenariosV2.Vehicle
 {
     public class Drone : Base
     {
+        #region Baselines
+        [Header("Adaption")] private BaselineS2 _baselineS2;
+        private BaselineS4 _baselineS4;
+
+        public BaselineS2 baselineS2
+        {
+            get
+            {
+                if (_baselineS2 == null)
+                {
+                    _baselineS2 = FindObjectOfType<BaselineS2>();
+                }
+
+                return _baselineS2;
+            }
+        }        
+
+        public BaselineS4 baselineS4
+        {
+            get
+            {
+                if (_baselineS4 == null)
+                {
+                    _baselineS4 = FindObjectOfType<BaselineS4>();
+                }
+
+                return _baselineS4;
+            }
+        }
+        #endregion
+
         #region Callbacks
 
         public void Drone_normals_1()
         {
             print("Invoked Drone_normals_1");
+            baselineS4.ShowWarning(2);
+            baselineS4.On_BaselineS4_2_Start();
         }
 
         public void Drone_normals_2()
         {
+            print("Invoked Drone_normals_2");
+            baselineS4.ShowWarning(3);
+            baselineS4.On_BaselineS4_3_Start();
+        }
 
+        public void Drone_normals_3() 
+        {
+            print("Invoked Drone_normals_3");
+            baselineS2.ShowWarning(2);
+            baselineS2.On_BaselineS2_2_Start();
+        } 
+        
+        public void Drone_normals_4() 
+        {
+            print("Invoked Drone_normals_4");
+            baselineS2.ShowWarning(3);
+            baselineS2.On_BaselineS2_3_Start();
         }
 
         public void Drone_accidents_1()
