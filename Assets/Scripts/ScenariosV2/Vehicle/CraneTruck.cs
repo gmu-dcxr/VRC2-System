568f4ddd38ee193c5cff9da5e71e14509c2c3dee
willfredranc <willfredranc@gmail.com>
2024-02-10 15:52:57 -0500

    edited CraneTruck.yml for incident revision. New testing scene for Ching Yu.

--- Assets/Scripts/ScenariosV2/Vehicle/CraneTruck.cs
+++ Assets/Scripts/ScenariosV2/Vehicle/CraneTruck.cs
@@ -7,16 +7,40 @@ namespace VRC2.ScenariosV2.Vehicle
 {
     public class CraneTruck : Base
     {
+        [Header("Adaption")] private BaselineS5 _baselineS5;
+
+        public BaselineS5 baselineS5
+        {
+            get
+            {
+                if (_baselineS5 == null)
+                {
+                    _baselineS5 = FindObjectOfType<BaselineS5>();
+                }
+
+                return _baselineS5;
+            }
+        }
         #region Callbacks
 
         public void CraneTruck_normals_1()
         {
             print("Invoked CraneTruck_normals_1");
+            baselineS5.ShowWarning(2);
+            baselineS5.On_BaselineS5_2_Start();
         }
 
         public void CraneTruck_normals_2()
         {
             print("Invoked CraneTruck_normals_2");
+            // crane truck unloads
+        }
+        
+        public void CraneTruck_normals_3()
+        {
+            print("Invoked CraneTruck_normals_3");
+            baselineS5.ShowWarning(3);
+            baselineS5.On_BaselineS5_3_Start();
         }
 
         public void CraneTruck_accidents_1()
@@ -24,34 +48,63 @@ namespace VRC2.ScenariosV2.Vehicle
             print("Invoked CraneTruck_accidents_1");
             // TODO: this is only for demonstration.
             // When the previous implementation BaselineSx is called, it will automatically show the expected warning.
-            warningController.ShowVisualOnly();
+            // warningController.ShowVisualOnly();
+            baselineS5.ShowWarning(2);
+            baselineS5.On_BaselineS5_2_Start();
         }
 
         public void CraneTruck_accidents_2()
         {
             print("Invoked CraneTruck_accidents_2");
+            //crane truck unloads
         }
 
         public void CraneTruck_accidents_3()
         {
             print("Invoked CraneTruck_accidents_3");
+            baselineS5.ShowWarning(3);
+            baselineS5.On_BaselineS5_3_Start();
         }
 
         public void CraneTruck_accidents_4()
         {
             print("Invoked CraneTruck_accidents_4");
+            baselineS5.ShowWarning(4);
+            baselineS5.On_BaselineS5_4_Start();
         }
 
         public void CraneTruck_accidents_5()
         {
             print("Invoked CraneTruck_accidents_5");
+            //cranetruck unloads
         }
 
         public void CraneTruck_accidents_6()
         {
             print("Invoked CraneTruck_accidents_6");
+            baselineS5.ShowWarning(5);
+            baselineS5.On_BaselineS5_5_Start();
         }
-
+        
+        public void CraneTruck_accidents_7()
+        {
+            print("Invoked CraneTruck_accidents_7");
+            baselineS5.ShowWarning(6);
+            baselineS5.On_BaselineS5_6_Start();
+        }
+        
+        public void CraneTruck_accidents_8()
+        {
+            print("Invoked CraneTruck_accidents_8");
+            //cranetruck unloads
+        }
+        
+        public void CraneTruck_accidents_9()
+        {
+            print("Invoked CraneTruck_accidents_9");
+            baselineS5.ShowWarning(7);
+            baselineS5.On_BaselineS5_7_Start();
+        }
         #endregion
     }
 }
