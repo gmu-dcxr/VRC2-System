443a6cbccf40f4822f0812a8fbcb86d291e755bd
michaelszilagyi <80373779+michaelszilagyi@users.noreply.github.com>
2024-02-12 10:45:52 -0500

    Scenario8 added

---
+++ Assets/Scripts/ScenariosV2/Scenario/Scenario8.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VRC2.ScenariosV2.Scenario
+{
+    public class Scenario8: Base
+    {
+        public void Start()
+        {
+            base.Start();
+            // update the private id variable
+            _id = 8;
+        }
+    }
+}
