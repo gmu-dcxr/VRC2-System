d2251920036cc21fd5bce0e1a786837a4e3c881c
Liuchuan Yu <lyu20@gmu.edu>
2024-02-14 16:47:56 -0500

    add Scenario9 and always use sheet pipe

---
+++ Assets/Scripts/ScenariosV2/Scenario/Scenario9.cs
@@ -0,0 +1,29 @@
+﻿namespace VRC2.ScenariosV2.Scenario
+{
+    public class Scenario9 : Base
+    {
+        public void Start()
+        {
+            base.Start();
+            // update the private id variable
+            _id = 9;
+        }
+        
+        public override void OnScenarioStart()
+        {
+            base.OnScenarioStart();
+            // The plan players receive at the beginning of the experiment is not doable on the construction site.
+        }
+
+        public override void OnScenarioFinish()
+        {
+            base.OnScenarioFinish();
+            // After they report it to the supervisor(Task>Incorrect instructions), they will receive a correct plan.
+        }
+
+        public void SetIncorrectInstruction()
+        {
+            
+        }
+    }
+}
