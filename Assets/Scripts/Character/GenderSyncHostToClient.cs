f58333aa4416536bcb9e7cbfb6e4364a532fa04d
Liuchuan Yu <lyu20@gmu.edu>
2024-02-26 11:12:34 -0500

    fix

--- Assets/Scripts/Character/GenderSyncHostToClient.cs
+++ Assets/Scripts/Character/GenderSyncHostToClient.cs
@@ -3,38 +3,16 @@ using System.Collections;
 using Fusion;
 using UnityEngine;
 
-using Timer = UnityTimer.Timer;
-
 namespace VRC2.Character
 {
     public class GenderSyncHostToClient : GenderSyncBase
     {
-        private Timer _timer;
-
         public void Start()
         {
             _playerSpawner = FindObjectOfType<PlayerSpawner>();
             synchronized = false;
         }
-
-        // Note: The reason why we need a timer here is that
-        // 1) Fusion network process in the following order:
-        //  a) Host: host in the scene
-        //  b) Join: joiner joins the scene, the scene in the host end will have two clones first, and the scene in
-        //      the joiner end may not have two clones. 
-        // 2) It won't work in the remote end if we directly call rpc without waiting.
-        // 3) Joiner to host is different because when the joiner side has two clones, the remote side (host) must have
-        //      already two clones.
-        void SetTimer(Action complete)
-        {
-            if (_timer != null)
-            {
-                Timer.Cancel(_timer);
-            }
-
-            _timer = Timer.Register(5.0f, complete, isLooped: false, useRealTime: true);
-        }
-
+        
         public void Update()
         {
             if (synchronized) return;
@@ -43,21 +21,17 @@ namespace VRC2.Character
             {
                 synchronized = true;
                 StartCoroutine(SendMessageAfter(5.0f));
-
-                // SetTimer(() =>
-                // {
-                //     var pid = GlobalConstants.localPlayer.PlayerId;
-                //     var female = GlobalConstants.playerGender == PlayerGender.Female;
-                //     var hair = GlobalConstants.playerHairIndex;
-                //     var skin = GlobalConstants.playerSkinIndex;
-                //
-                //     print($"GenderSyncHostToClient: {pid} {female} {hair} {skin}");
-                //     // send message
-                //     RPC_SendMessage(pid, female, hair, skin);
-                // });
             }
         }
-
+        
+        // Note: The reason why we need a wait here is that
+        // 1) Fusion network process in the following order:
+        //  a) Host: host in the scene
+        //  b) Join: joiner joins the scene, the scene in the host end will have two clones first, and the scene in
+        //      the joiner end may not have two clones. 
+        // 2) It won't work in the remote end if we directly call rpc without waiting.
+        // 3) Joiner to host is different because when the joiner side has two clones, the remote side (host) must have
+        //      already two clones.
         IEnumerator SendMessageAfter(float second)
         {
             yield return new WaitForSeconds(second);
