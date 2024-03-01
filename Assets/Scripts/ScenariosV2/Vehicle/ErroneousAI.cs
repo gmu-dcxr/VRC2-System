4414610dfd4876350da68e0844632ffbbe4df949
Liuchuan Yu <lyu20@gmu.edu>
2024-03-01 10:45:05 -0500

    add ErroneousAI

---
+++ Assets/Scripts/ScenariosV2/Vehicle/ErroneousAI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using VRC2.Scenarios.ScenarioFactory;
+using VRC2.ScenariosV2.Tool;
+
+namespace VRC2.ScenariosV2.Vehicle
+{
+    public class ErroneousAI : Base
+    {
+        #region Callbacks
+
+        public void ErroneousAI_accidents_1(object[] parameters)
+        {
+            print("Invoked ErroneousAI_accidents_1");
+        }
+
+
+        public void ErroneousAI_accidents_2(object[] parameters)
+        {
+            print("Invoked ErroneousAI_accidents_2");
+        }
+
+
+        public void ErroneousAI_accidents_3(object[] parameters)
+        {
+            print("Invoked ErroneousAI_accidents_3");
+        }
+
+
+        public void ErroneousAI_accidents_4(object[] parameters)
+        {
+            print("Invoked ErroneousAI_accidents_4");
+        }
+
+
+        public void ErroneousAI_accidents_5(object[] parameters)
+        {
+            print("Invoked ErroneousAI_accidents_5");
+        }
+        
+        #endregion
+    }
+}
