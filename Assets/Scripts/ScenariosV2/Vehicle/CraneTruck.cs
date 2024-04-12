9e140c8f387aa05b50b94d3276220d11c113284e
Liuchuan Yu <lyu20@gmu.edu>
2024-04-12 18:49:20 -0400

    fix cranetruck

--- Assets/Scripts/ScenariosV2/Vehicle/CraneTruck.cs
+++ Assets/Scripts/ScenariosV2/Vehicle/CraneTruck.cs
@@ -21,12 +21,16 @@ namespace VRC2.ScenariosV2.Vehicle
                 return _baselineS5;
             }
         }
+
         #region Callbacks
+
+        // TODO: WARNING: warning sound defined in cranetruck rather than in BaselineS5
 
         public void CraneTruck_normals_1(object[] parameters)
         {
             BluePrint("Invoked CraneTruck_normals_1");
-            baselineS5.ShowWarning(2);
+            // baselineS5.ShowWarning(2);
+            PlayAudioOnly(true, 1);
             baselineS5.On_BaselineS5_2_Start();
         }
 
@@ -34,8 +38,9 @@ namespace VRC2.ScenariosV2.Vehicle
         {
             BluePrint("Invoked CraneTruck_normals_2");
             // crane truck unloads
+            PlayAudioOnly(true, 2);
         }
-        
+
         public void CraneTruck_normals_3(object[] parameters)
         {
             BluePrint("Invoked CraneTruck_normals_3");
@@ -49,7 +54,8 @@ namespace VRC2.ScenariosV2.Vehicle
             // TODO: this is only for demonstration.
             // When the previous implementation BaselineSx is called, it will automatically show the expected warning.
             // warningController.ShowVisualOnly();
-            baselineS5.ShowWarning(2);
+            // baselineS5.ShowWarning(2);
+            PlayAudioOnly(false, 1);
             baselineS5.On_BaselineS5_2_Start();
         }
 
@@ -57,6 +63,7 @@ namespace VRC2.ScenariosV2.Vehicle
         {
             BluePrint("Invoked CraneTruck_accidents_2");
             //crane truck unloads
+            PlayAudioOnly(false, 2);
         }
 
         public void CraneTruck_accidents_3(object[] parameters)
@@ -69,7 +76,8 @@ namespace VRC2.ScenariosV2.Vehicle
         public void CraneTruck_accidents_4(object[] parameters)
         {
             BluePrint("Invoked CraneTruck_accidents_4");
-            baselineS5.ShowWarning(4);
+            // baselineS5.ShowWarning(4);
+            PlayAudioOnly(false, 4);
             baselineS5.On_BaselineS5_4_Start();
         }
 
@@ -77,6 +85,7 @@ namespace VRC2.ScenariosV2.Vehicle
         {
             BluePrint("Invoked CraneTruck_accidents_5");
             //cranetruck unloads
+            PlayAudioOnly(false, 5);
         }
 
         public void CraneTruck_accidents_6(object[] parameters)
@@ -85,26 +94,29 @@ namespace VRC2.ScenariosV2.Vehicle
             baselineS5.ShowWarning(5);
             baselineS5.On_BaselineS5_5_Start();
         }
-        
+
         public void CraneTruck_accidents_7(object[] parameters)
         {
             BluePrint("Invoked CraneTruck_accidents_7");
-            baselineS5.ShowWarning(6);
+            // baselineS5.ShowWarning(6);
+            PlayAudioOnly(false, 7);
             baselineS5.On_BaselineS5_6_Start();
         }
-        
+
         public void CraneTruck_accidents_8(object[] parameters)
         {
             BluePrint("Invoked CraneTruck_accidents_8");
             //cranetruck unloads
+            PlayAudioOnly(false, 8);
         }
-        
+
         public void CraneTruck_accidents_9(object[] parameters)
         {
             BluePrint("Invoked CraneTruck_accidents_9");
             baselineS5.ShowWarning(7);
             baselineS5.On_BaselineS5_7_Start();
         }
+
         #endregion
     }
 }
