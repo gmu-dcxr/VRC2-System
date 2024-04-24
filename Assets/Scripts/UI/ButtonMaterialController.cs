0de4ffbe7c805140a22a8e39d68dffc9743d37c2
Liuchuan Yu <lyu20@gmu.edu>
2024-04-24 17:20:37 -0400

    update drone ui

--- Assets/Scripts/UI/ButtonMaterialController.cs
+++ Assets/Scripts/UI/ButtonMaterialController.cs
@@ -2,32 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VRC2.ScenariosV2.Tool;
 
 namespace VRC2
 {
 
     public class ButtonMaterialController : MonoBehaviour
     {
-        [Header("Appearance")] public Material defaultMaterial;
+        [ReadOnly] public Color selectedColor = Color.white;
 
-        public Material selectedMaterial;
+        [Header("Image-Based")] public bool imageBased = false;
+        public Sprite selectedImage;
+        private Sprite originalImage;
 
+        private Color originalColor;
         private Button _button;
 
-        public Material currentMaterial
+        private Image _image;
+
+        // if it's being selected
+        public bool isSelected
         {
             get
             {
-                return gameObject.GetComponent<Image>().material;
+                if (imageBased)
+                {
+                    return _image.sprite == selectedImage;
+                }
+                else
+                {
+                    return _image.color == selectedColor;
+                }
             }
         }
 
         // Start is called before the first frame update
         void Start()
         {
-            _button = gameObject.GetComponent<Button>();
+            _button = GetComponent<Button>();
             // bind event
             _button.onClick.AddListener(ChangeMaterial);
+
+            _image = GetComponent<Image>();
+            originalColor = _image.color;
+
+            originalImage = _image.sprite;
         }
 
         // Update is called once per frame
@@ -36,20 +55,32 @@ namespace VRC2
 
         }
 
+        // change material by changing color
         public void ChangeMaterial()
         {
-            // get button image
-            var image = gameObject.GetComponent<Image>();
-
-            if (image.material == selectedMaterial)
+            if (imageBased)
             {
-                image.material = defaultMaterial;
+                if (_image.sprite != selectedImage)
+                {
+                    _image.sprite = selectedImage;
+                }
+                else
+                {
+                    _image.sprite = originalImage;
+                }
             }
             else
             {
-                image.material = selectedMaterial;
+                // color based: only change color
+                if (_image.color != selectedColor)
+                {
+                    _image.color = selectedColor;
+                }
+                else
+                {
+                    _image.color = originalColor;
+                }
             }
         }
-        
     }
 }
