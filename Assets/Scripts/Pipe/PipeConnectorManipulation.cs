b3718059ead934901ee1974253a80dc3db8556a9
Liuchuan Yu <lyu20@gmu.edu>
2024-05-13 16:53:50 -0400

    add PipeConnectorManipulation

---
+++ Assets/Scripts/Pipe/PipeConnectorManipulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Oculus.Interaction;
+using UnityEngine;
+
+namespace VRC2.Pipe
+{
+    [RequireComponent(typeof(PointableUnityEventWrapper))]
+    public class PipeConnectorManipulation : MonoBehaviour
+    {
+        private PointableUnityEventWrapper _wrapper;
+
+        private Rigidbody _rigidbody
+        {
+            get => GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            _wrapper = gameObject.GetComponent<PointableUnityEventWrapper>();
+            _wrapper.WhenSelect.AddListener(OnSelect);
+            _wrapper.WhenRelease.AddListener(OnRelease);
+        }
+
+        public void OnSelect()
+        {
+            // enable kinematic
+            _rigidbody.isKinematic = true;
+        }
+
+        public void OnRelease()
+        {
+            // disable kinematic
+            _rigidbody.isKinematic = false;
+        }
+    }
+}
