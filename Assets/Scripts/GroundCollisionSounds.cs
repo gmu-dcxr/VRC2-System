5671de8f415172c89239aa1c93387222ae9fa7db
j-dow7 <jdow7@gmu.edu>
2024-06-12 15:44:34 -0400

    Ground collision sounds

---
+++ Assets/Scripts/GroundCollisionSounds.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollisionSounds : MonoBehaviour
+{
+    public AudioSource collisionNoise;
+
+    void Start()
+    {
+        collisionNoise = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground") 
+        {
+            collisionNoise.Play();
+            print("working");
+        }
+    }
+}
